5188ce39107334bb1919fce71608cbb453d02004
innovateccenter@gmail.com <innovateccenter@gmail.com>
2026-06-01 16:42:23 -0500

    PRIMER ENTREGABLE, EL DISEÑO GENERAL DE LA VENTANA

---
+++ acceso.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+
+ventana = tk.Tk()
+ventana.title("Acceso al Sistema de Soporte Técnico")
+ventana.geometry("400x600+550+100")
+ventana.resizable(False,False)
+
+
+ventana.mainloop()
